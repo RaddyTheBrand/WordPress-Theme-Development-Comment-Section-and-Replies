a2e752fc793d4b0401ff60cb80df1195386a4e73
Radoslav Angelov <rx3mer@live.co.uk>
2020-05-27 10:15:59 +0100

    Tutorial Files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WordPress Theme Development Comment Section and Replies
 WordPress Comment Section and Replies
+
+[Link to YouTube Video!](https://www.youtube.com/watch?v=8MUfp86r7pY)
---
+++ comments.php
@@ -0,0 +1,5 @@
+<div class="minimalist_comments">
+	<ol class="commentlist">
+	<?php wp_list_comments( 'type=comment&callback=theminimalist_comment&avatar_size=62&max_depth=2' ); ?>
+	</ol>
+</div>
